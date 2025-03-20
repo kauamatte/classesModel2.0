ec23e8f388624b0fbab5e0bade8be9a32bdc6ee6
Giovana <bgiovana622@gmail.com>
2025-03-20 13:08:59 -0300

    Service

---
+++ classesmodel/src/main/java/br/edu/soph/classes/classesmodel/service/MovieService.java
@@ -0,0 +1,37 @@
+package br.edu.soph.classes.classesmodel.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import br.edu.soph.classes.classesmodel.Model.Movie;
+import br.edu.soph.classes.classesmodel.Repository.MovieRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+
+@Service
+public class MovieService {
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Transactional
+    public Movie register(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    public List<Movie> findAll() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findById(UUID id) {
+        return movieRepository.findById(id);
+    }
+
+    @Transactional
+    public void remove(UUID id) {
+        movieRepository.deleteById(id);
+    }
+}
